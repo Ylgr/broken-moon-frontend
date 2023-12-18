d14551e2c547accc46b86a8c0be555cb05bcde28
viet tu <quyen.luminous@gmail.com>
2023-12-18 19:21:24 +0700

    Updates

---
+++ static/js/38.d46d3ab6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonplightence-admin"]=this["webpackJsonplightence-admin"]||[]).push([[38],{2600:function(e,c,n){"use strict";n.r(c);n(0);var t=n(60),i=n(41),s=n(2);c.default=function(){return Object(s.jsx)(i.r,{children:Object(s.jsx)(i.d,{children:Object(s.jsx)(i.m,{children:Object(s.jsx)(t.b,{})})})})}}}]);
+//# sourceMappingURL=38.d46d3ab6.chunk.js.map
---
+++ static/js/39.e669bfba.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonplightence-admin"]=this["webpackJsonplightence-admin"]||[]).push([[39],{2621:function(t,e,c){"use strict";c.r(e);var n=c(0),a=c(33),i=c(60),s=c(123),u=c(2);e.default=function(){var t=Object(a.a)();return Object(n.useEffect)((function(){t(Object(s.c)())}),[t]),Object(u.jsx)(i.a,{to:"/auth/login",replace:!0})}}}]);
+//# sourceMappingURL=39.e669bfba.chunk.js.map
